1cbc4d35bf8e8d14a5bbd5e38ca970d8d2e28d17
mbappe0310 <macario_an@naver.com>
2018-08-18 16:28:08 +0900

    몬스터 제너레이터 코딩 작업함

--- Team2P/Assets/Scripts/MonsterGenerator.cs
+++ Team2P/Assets/Scripts/MonsterGenerator.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MonsterGenerator : MonoBehaviour {
     
     // 몬스터를 새로 만들어내는 역할
-    public GameObject monster;
-    private float TimeLeft = 3.0f;
-    private float nextTime = 0.0f;
+    public GameObject[] monster;
+    private float interval = 5.0f;
+    private float currentTime = 0.0f;
 	// Use this for initialization
 	void Start () {
 
@@ -15,11 +16,58 @@ public class MonsterGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time > nextTime)
+        currentTime += Time.deltaTime;
+        if(currentTime >= interval)
         {
-            nextTime = TimeLeft * Time.deltaTime;
-            Vector3 newPosition = new Vector3(Random.Range(-6f, -6f), Random.Range(-5f, 5f), -1);
-            Instantiate(monster, newPosition, Quaternion.identity);
+            int randtime = (int)Random.Range(0, 4);
+            if (randtime == 0)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {   
+                    Vector3 newPosition = new Vector3(Random.Range(-19.5f, 19.5f), Random.Range(19.5f, 19.5f), -1);
+                    Instantiate(monster[1], newPosition, Quaternion.identity);
+                }
+
+            }
+            if (randtime == 1)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Vector3 newPosition = new Vector3(Random.Range(20f, 20f), Random.Range(18f, -18f), -1);
+                    Instantiate(monster[0], newPosition, Quaternion.identity);
+                }
+
+            }
+            if (randtime == 2)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Vector3 newPosition = new Vector3(Random.Range(-18.9f, -18.9f), Random.Range(18.2f, -18.2f), -1);
+                    Instantiate(monster[2], newPosition, Quaternion.identity);
+                }
+
+            }
+            if (randtime == 3)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Vector3 newPosition = new Vector3(Random.Range(-18.82f, 18.82f), Random.Range(-19.51f, -19.51f), -1);
+                    Instantiate(monster[3], newPosition, Quaternion.identity);
+                }
+
+            }
+            currentTime = 0;
         }
-	}
+        //if (Time.time > nextTime)
+        //{
+        //    nextTime = TimeLeft * Time.deltaTime;
+        //    Vector3 newPosition = new Vector3(Random.Range(-19.5f, 19.5f), Random.Range(19.5f, 19.5f), -1);
+        //    Instantiate(monster[0], newPosition, Quaternion.identity);
+        //}
+
+    }
 }
