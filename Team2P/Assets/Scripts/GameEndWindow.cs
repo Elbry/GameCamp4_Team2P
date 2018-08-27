0d4ad00d6175ef4e519a56c5aac45ccb2f04b6fa
cahill1502 <cahill1502@naver.com>
2018-08-27 19:13:23 +0900

    게임 종료 창 제작.

---
+++ Team2P/Assets/Scripts/GameEndWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameEndWindow : MonoBehaviour
+{
+    public Text textKillCount = null;
+    public Text textPlayTime = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    public void SetGameEndInfo(int killCount, float playTime)
+    {
+        textKillCount.text = killCount.ToString();
+        textPlayTime.text = playTime.ToString();
+    }
+
+    public void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+}   
--- Team2P/Assets/Scripts/GameManager.cs
+++ Team2P/Assets/Scripts/GameManager.cs
@@ -2,39 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 	// 게임의 시작, 일시정지, 종료 등을 담당
 	Timer timer;
 	public int goalTime;
     public float score = 0f;
+    protected int nKillCount = 0;
 
-	// Use this for initialization
-	void Start () {
-		timer = FindObjectOfType<Timer>();
+    float fStartGameTime = 0.0f;
+
+    public GameObject gameEndWindow = null;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        HideGameEndWindow();
+        nKillCount = 0;
+
+        timer = FindObjectOfType<Timer>();
 		GameStart();
 		timer.gameObject.SetActive(true);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(timer.currentTime >= goalTime) {
+	void Update ()
+    {
+		if(timer.currentTime >= goalTime)
+        {
 			GameClear();
 		}
+        
+        // Test Code
+        if( Input.GetKeyUp(KeyCode.A) )
+        {
+            nKillCount = 10;
+            ShowGameEndWindow();
+        }
 	}
 
-	void GameStart() {
+	void GameStart()
+    {
+        // 게임 시작 시간을 저장한다.
+        fStartGameTime = Time.time;
+	}
+
+	void GamePause()
+    {
 
 	}
 
-	void GamePause() {
+	void GameOver()
+    {
+        ShowGameEndWindow();
+    }
+
+	void GameClear()
+    {
 
 	}
 
-	void GameOver() {
+    void HideGameEndWindow()
+    {
+        gameEndWindow.SetActive(false);   // 게임 종료 화면을 비활성화 한다.
+    }
 
-	}
+    void ShowGameEndWindow()
+    {
+        float fPlayTime = Time.time - fStartGameTime;
+        gameEndWindow.GetComponent<GameEndWindow>().SetGameEndInfo(nKillCount, fPlayTime);
+        gameEndWindow.SetActive(true);        // 게임 종료 화면을 활성화 한다.
+    }
 
-	void GameClear() {
-
-	}
+    // 죽인 적의 수를 1증가 시킨다.
+    public void IncreaseKillCount()
+    {
+        nKillCount++;
+    }
 }
